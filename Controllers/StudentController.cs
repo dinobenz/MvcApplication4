e6f9b787417e34ae04bffdda6404a8f4a2df992f
Pakorn Weecharungsan <dinobenz@hotmail.com>
2013-11-11 22:28:04 +0700

    initial commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using MvcApplication4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication4.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /Student/
+
+        public ActionResult Index()
+        {
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student()
+                {
+                    ID = 5601,
+                    Name = "Name A"
+                });
+            
+            students.Add(new Student()
+            {
+                ID = 5602,
+                Name = "Name B"
+            });
+
+            students.Add(new Student()
+            {
+                ID = 5603,
+                Name = "Name C"
+            });
+
+            return View(students.ToArray());
+        }
+
+    }
+}
